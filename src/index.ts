d0ddd0a6df15a76055d0f443ac6db4636c20ddf4
John Bioux <john.bioux@gmail.com>
2020-10-14 21:42:06 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -12,58 +12,56 @@ const api_url = "https://api.giphy.com/v1/gifs/search";
 const api_key = "99CkiDwofPOVZFGBTNnEHU7paQ9SVCJx";
 
 const convertImage = (frame: string): Promise<string> =>
-	new Promise((resolve, reject) => {
-		imageToAscii(frame, (err: any, converted: any) => {
-			if (err) {
-				console.log(err);
-				reject(err);
-			}
-			resolve(converted);
-		});
-	});
+  new Promise((resolve, reject) => {
+    imageToAscii(frame, (err: any, converted: any) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      resolve(converted);
+    });
+  });
 
 const main = async () => {
-	// @ts-ignore
-	await fs.rmdir("gif", { recursive: true });
-	const args = process.argv.splice(2);
-	const q = args[0];
-	if (!q) {
-		console.log("you need to enter search");
-		return;
-	}
-	const { data } = await axios.get(api_url, {
-		params: {
-			api_key,
-			q,
-			lang: "fr",
-		},
-	});
-	const a = await inquirer.prompt([
-		{
-			type: "list",
-			message: "choose a gif",
-			name: "url",
-			choices: data.data.map((item: any) => ({
-				name: item.title.trim() !== "" ? item.title : item.slug,
-				value: item.images.original.url,
-			})),
-		},
-	]);
-	await fs.ensureDir(path.join("gif", "frames"));
-	await explodeGif(a.url);
-	const n = (await fs.readdir("gif/frames")).length;
-	const frames: string[] = [];
-	for (let i = 0; i < n; i++) {
-		const frame = await convertImage(`./gif/frames/frame-${i}.jpeg`);
-		frames.push(frame);
-	}
-	var animation = new CliFrames();
-	animation.load(frames);
-	animation.start({
-		repeat: true,
-		delay: 100,
-	});
+  // @ts-ignore
+  await fs.rmdir("gif", { recursive: true });
+  const args = process.argv.splice(2);
+  const q = args[0];
+  if (!q) {
+    console.log("you need to enter search");
+    return;
+  }
+  const { data } = await axios.get(api_url, {
+    params: {
+      api_key,
+      q,
+      lang: "fr",
+    },
+  });
+  const a = await inquirer.prompt([
+    {
+      type: "list",
+      message: "choose a gif",
+      name: "url",
+      choices: data.data.map((item: any) => ({
+        name: item.title.trim() !== "" ? item.title : item.slug,
+        value: item.images.original.url,
+      })),
+    },
+  ]);
+  await fs.ensureDir(path.join("gif", "frames"));
+  await explodeGif(a.url);
+  const files = await fs.readdir("gif/frames");
+  const frames = await Promise.all(
+    files.map((item) => convertImage(`./gif/frames/${item}`))
+  );
+  var animation = new CliFrames();
+  animation.load(frames);
+  animation.start({
+    repeat: true,
+    delay: 50,
+  });
 
-	return true;
+  return true;
 };
 main();
